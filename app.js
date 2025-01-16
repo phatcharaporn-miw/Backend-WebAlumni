d28fb39df60f93905177e4fc98c5f4f50748d973
phatcharaporn-miw24 <phatcharaporn.ni@kkumail.com>
2025-01-16 20:10:56 +0700

    gg

--- app.js
+++ app.js
@@ -48,16 +48,16 @@ app.use(session({
 
 
 // ตั้งค่าช่องทางการเชื่อมต่อ OAuth 2.0
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3001/auth/google/callback"
-}, function(token, tokenSecret, profile, done) {
-  if (!profile) {
-    return done(new Error('Profile not found'));
-  }
-  return done(null, profile);
-}));
+// passport.use(new GoogleStrategy({
+//   clientID: process.env.GOOGLE_CLIENT_ID,
+//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//   callbackURL: "http://localhost:3001/auth/google/callback"
+// }, function(token, tokenSecret, profile, done) {
+//   if (!profile) {
+//     return done(new Error('Profile not found'));
+//   }
+//   return done(null, profile);
+// }));
 
 app.use(passport.initialize());
 app.use(passport.session());
